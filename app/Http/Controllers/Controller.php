41b05552679fce35532770ab4e864d2b1f3db6e6
taii0401 <taii0401@gmail.com>
2021-12-14 00:29:52 +0800

    [mod]加上共用函式

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,35 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+use Illuminate\Support\Str;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    //印出資料
+    public function pr($data,$ret=false)
+    {
+        echo "<pre>";print_r($data,$ret);echo "</pre>";
+    }
+
+    //取得數字和字母隨機位數
+    public function getRandom($num)
+    {
+        $ran_str = "";
+        for($i = 0;$i < $num;$i++) {
+            //定義一個隨機範圍，去猜i的值
+            $current = rand(0,$num);
+            if($current == $i) {                                
+                //生成一個隨機的數字
+                $current_code = rand(0,9);
+            } else {
+                //生成一個隨機的字母
+                $current_code = Str::random(1);
+            }
+            $ran_str .= $current_code;
+        }
+        return $ran_str;
+    }
+    
 }
