585491d4a0789bbcda4a6905ee6b933ef5c5ee0d
taii0401 <taii0401@gmail.com>
2021-12-22 21:08:02 +0800

    [mod][使用者]刪除多餘的程式碼

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Crypt;
-
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 
 use App\Libraries\UserAuth;
@@ -14,12 +11,7 @@ use App\Models\UnshopUser;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    //登入
+    //首頁-登入
     public function index()
     {
         //判斷是否登入
@@ -30,11 +22,6 @@ class UserController extends Controller
         return view("users.index");
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     //新增使用者
     public function create()
     {
@@ -47,34 +34,7 @@ class UserController extends Controller
         return view("users.data",$data);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    //編輯使用者
     public function edit($id=0)
     {
         //判斷是否登入
@@ -84,29 +44,6 @@ class UserController extends Controller
         }
         $data = $this->get_data("edit",session("userUuid"));
         return view("users.data",$data);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 
     //共用data樣板-新增使用者、編輯使用者、修改密碼
