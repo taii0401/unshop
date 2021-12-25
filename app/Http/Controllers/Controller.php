f678f276219275b2c49a929916a268171d1d59c4
taii0401 <taii0401@gmail.com>
2021-12-25 22:37:23 +0800

    [mod][controller]加上處理分頁資料

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 use Illuminate\Support\Str;
+use Illuminate\Pagination\Paginator;
 
 class Controller extends BaseController
 {
@@ -38,4 +39,35 @@ class Controller extends BaseController
         return $ran_str;
     }
     
+    //分頁
+    public function getPage($page_link="",$page=1,$datas)
+    {
+        $page_data = array();
+        //頁面連結
+        $page_data["page_link"] = $page_link;
+    
+        $paginator = $datas->paginate(env("GLOBAL_PAGE_NUM"));
+        //資料總數
+        $page_data["count"] = $paginator->total();
+        //總頁數
+        $last_page = $paginator->lastPage();
+        $page_data["last_page"] = $last_page;
+        //目前頁數
+        $page_data["page"] = $page;
+        //前一頁的頁碼
+        $page_data["previous_page_number"] = 1;
+        if($page != 1) {
+            $page_data["previous_page_number"] = $page-1;
+        }
+        //後一頁的頁碼
+        $page_data["next_page_number"] = $last_page;
+        if($page < $last_page) {
+            $page_data["next_page_number"] = $page+1;
+        }
+        //目前頁面資料
+        $list_datas = $paginator->toArray();
+        $page_data["list_data"] = isset($list_datas["data"])?$list_datas["data"]:array();
+    
+        return $page_data;
+    }
 }
