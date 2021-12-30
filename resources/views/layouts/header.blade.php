6d2b8ab2295be40c787a937d90a8993c554a87ff
taii0401 <taii0401@gmail.com>
2021-12-30 23:20:38 +0800

    [mod][樣板]修改選單

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,16 +12,21 @@
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mx-auto">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">會員管理</a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="/users/edit_password">修改密碼</a>
-                            <a class="dropdown-item" href="/users/edit">會員資料</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/products">商品管理</a>
-                    </li>
+                    @if(UserAuth::isLoggedIn())
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">會員管理</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="/users/edit_password">修改密碼</a>
+                                <a class="dropdown-item" href="/users/edit">會員資料</a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/products">商品管理</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/fronts/my_page/{{ UserAuth::userdata()->short_link }}">我的頁面</a>
+                        </li>
+                    @endif
                 </ul>
                 <ul class="navbar-nav">
                     <li class="nav-item">
